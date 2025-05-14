1df13aa9096553fb04060afa364cfa4ad6ef8157
OnionTree <lesstory01@gmail.com>
2025-05-15 00:00:53 +0900

    스테이지 이동 구현
    다음 스테이지로 이동 시 카메라 경계 변경하는 효과 추가.
    (이전 스테이지로 복귀 시 미작동)

--- TestFlatformer/Assets/Scripts/LES/Play/GameManager.cs
+++ TestFlatformer/Assets/Scripts/LES/Play/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     public int stageLevel = 0;
     public List<StageSO> stageList = new List<StageSO>();
 
-    public Vector2 playerVector;
+    public Transform characterTransform;
 
     public CameraFollow CameraFollow;
 
@@ -20,9 +20,37 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        CameraBoundSetting();
+        characterTransform.position = stageList[stageLevel].start;
+    }
+
+    private void Update()
+    {
+        Debug.Log(characterTransform.position.x);
+        Debug.Log(stageList[stageLevel].maxCameraBounds.x);
+        if (characterTransform.position.x < stageList[stageLevel].minCameraBounds.x ||
+            characterTransform.position.x > stageList[stageLevel].maxCameraBounds.x ||
+            characterTransform.position.y < stageList[stageLevel].minCameraBounds.y ||
+            characterTransform.position.y > stageList[stageLevel].maxCameraBounds.y)
+        {   // 카메라 범위에서 캐릭터가 벗어나면
+            Debug.Log("스테이지 진행");
+            int x = stageLevel + 1;
+            Debug.Log(stageList[x].maxCameraBounds.x);
+            if (characterTransform.position.x > stageList[x].minCameraBounds.x &&
+                characterTransform.position.x < stageList[x].maxCameraBounds.x &&
+                characterTransform.position.y > stageList[x].minCameraBounds.y &&
+                characterTransform.position.y < stageList[x].maxCameraBounds.y)
+            {
+                Debug.Log("진행 성공");
+                stageLevel = x;
+                CameraBoundSetting();
+            }
+        }
+    }
+
+    private void CameraBoundSetting()
+    {
         CameraFollow.minBounds = stageList[stageLevel].minCameraBounds;
-        Debug.Log(stageList[stageLevel].name);
-        Debug.Log(stageList[stageLevel].minCameraBounds);
         CameraFollow.maxBounds = stageList[stageLevel].maxCameraBounds;
     }
 }
--- TestFlatformer/Assets/Scripts/LES/Play/StageSO.cs
+++ TestFlatformer/Assets/Scripts/LES/Play/StageSO.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 public class StageSO : ScriptableObject
 {
     public int level;
-    public float startX;
-    public float startY;
+    public Vector2 start = new Vector2();
 
     public Vector2 minCameraBounds = new Vector2(0, -10);
     public Vector2 maxCameraBounds = new Vector2(0, 10);
